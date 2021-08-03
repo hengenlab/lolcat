518c94a4bd2a60991900c481b37e4cd01123e38c
mazabou <36160333+mazabou@users.noreply.github.com>
2021-08-03 11:28:12 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
-
-from .data import Dataset, FiringRates, ISIDistribution, ConcatFeats
+from .data import Dataset
 from . import covariance
 from . import network
 from .pytorch_dataset import EdgeDistributionDataset, ISIDistributionDataset, FRDistributionDataset, ISIFRDistributionDataset
