8d1cd62a3a8792a53205e06b63cf6c9f38a7440e
Mehdi Azabou <mehdiazabou@gmail.com>
2021-09-05 22:23:01 -0400

    .gitignore fix

--- src/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from .data import Dataset, FiringRates, ISIDistribution, ConcatFeats
-from . import covariance
-from . import network
-from .pytorch_dataset import EdgeDistributionDataset, ISIDistributionDataset, FRDistributionDataset, ISIFRDistributionDataset
